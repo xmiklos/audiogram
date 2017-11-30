4536fb66458cfbe0216df57890b3c2b40dafca9b
Michal Mikloš <xmiklos@fi.muni.cz>
2017-11-30 14:41:49 +0100

    progress

--- js/app.js
+++ js/app.js
@@ -103,13 +103,14 @@ a.Define.module('Audiogram', function(ns, $){
 	var ctx = canvas.getContext("2d");
 	var table_x = 50;
 	var table_y = 100;
-	var size = 35;
+	var size = 30;
 	var font_size = 12;
 	var khz = [60, 125, 250, 500, 1000, 2000, 4000, 8000, 12000];
 	
 	return {
 		drawLayout: function() {
-			
+
+			ctx.beginPath();
 			ctx.lineWidth=3;
 			ctx.moveTo(table_x, table_y);
 			ctx.lineTo(table_x, table_y + 12 * size);
@@ -117,13 +118,15 @@ a.Define.module('Audiogram', function(ns, $){
 			ctx.lineTo(table_x + 8 * size * 2, table_y);
 			ctx.lineTo(table_x, table_y);
 			ctx.stroke();
+			ctx.closePath();
 			
 			for (var i = 1; i < 12; ++i) {
 				ctx.beginPath();
-				ctx.lineWidth=1;
+				ctx.lineWidth=1.5;
 				ctx.moveTo(table_x, table_y + size * i);
 				ctx.lineTo(table_x + 8 * size * 2, table_y + size * i);
 				ctx.stroke();
+				ctx.closePath();
 			}
 			
 			for (var i = 1; i < 8; ++i) {
@@ -132,6 +135,7 @@ a.Define.module('Audiogram', function(ns, $){
 				ctx.moveTo(table_x + 2*size * i, table_y);
 				ctx.lineTo(table_x + 2*size * i, table_y + size * 12);
 				ctx.stroke();
+				ctx.closePath();
 			}
 			
 			ctx.font= font_size+"px Arial";
@@ -139,8 +143,8 @@ a.Define.module('Audiogram', function(ns, $){
 			
 			for (var i = -10, j = 0; i <= 100 ; i+=10, j++) {
 				var txt = ''+i;
-				ctx.fillText(txt, table_x - 10 - ctx.measureText(txt).width, table_y + size * j + (size / 2));
-				ctx.fillText(txt, table_x + 8 * 2 * size + 25 - ctx.measureText(txt).width, table_y + size * j + (size / 2));
+				ctx.fillText(txt, table_x - 10 - ctx.measureText(txt).width, table_y + size * j);
+				ctx.fillText(txt, table_x + 8 * 2 * size + 25 - ctx.measureText(txt).width, table_y + size * j);
 			}
 			
 			ctx.textBaseline="bottom";
@@ -148,33 +152,97 @@ a.Define.module('Audiogram', function(ns, $){
 				var txt = ''+khz[j];
 				ctx.fillText(txt, table_x + j * size * 2 - ctx.measureText(txt).width / 2, table_y - 5);
 			}
-			
-			ctx.save();
 		},
 		reDraw: function() {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			this.clearLayout();
 			this.drawLayout();
-			for (var i = 1; i < khz.length - 1; ++i) {
-				var db = $('#vv_'+khz[i]+'_input').val();
-				_drawX(table_x + i + 2 * size, table_y + size + (db/10)*size);
+			var line = false;
+			var prefixes = ['vv_', 'pv_', 'vk_', 'pk_'];
+			var fncs = [_drawX, _drawO, _drawRS, _drawLS];
+			
+			for (var j = 0; j < prefixes.length; ++j) {
+				var prefix = prefixes[j];
+				for (var i = 1; i < khz.length - 1; ++i) {
+					var db = $('#'+prefix+khz[i]+'_input').val();
+					var enabled = $('.'+prefix+khz[i]+'_en').is(':checked');
+					if (enabled) {
+						var x = table_x + i * 2 * size;
+						var y = table_y + size + (db/10)*size;
+					
+						if (line) { // end path from prev iteration
+							ctx.lineTo(x - 7, y - 3);
+							ctx.stroke();
+							ctx.closePath();
+						}
+
+						fncs[j](x, y);
+
+						line = i + 1 < khz.length - 1 && $('.'+prefix+khz[i + 1]+'_en').is(':checked'); // is there line to next?
+
+						if (line) {
+							ctx.beginPath();
+							ctx.lineWidth=2;
+							ctx.moveTo(x + 7, y - 3);
+						}
+					}
+				}
 			}
+		},
+		clearLayout: function() {			
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		},
+		downloadPDF: function() {
+			var pdf = new jsPDF();
+			var width = pdf.internal.pageSize.width;    
+			var height = pdf.internal.pageSize.height;
+			pdf.addImage(canvas.toDataURL("image/png"),"PNG", 0, 0, width, height / 2);
+			pdf.save("test.pdf");
 		},
 	};
 	
 	function _drawX(x, y) {
+	/*
 		ctx.beginPath();
-		ctx.lineWidth=1;
-		ctx.moveTo(x - 10, y - 10);
-		ctx.lineTo(x + 10, y + 10);
-		ctx.moveTo(x - 10, y + 10);
-		ctx.lineTo(x + 10, y - 10);
+		ctx.lineWidth=1.5;
+		var c = 6;
+		ctx.moveTo(x - c, y - c);
+		ctx.lineTo(x + c, y + c);
+		ctx.moveTo(x - c, y + c);
+		ctx.lineTo(x + c, y - c);
 		ctx.stroke();
+		ctx.closePath();
+	*/
+		ctx.font = "bold "+(font_size+10)+"px Arial";
+		ctx.textBaseline = "middle";
+		var txt = "X";
+		ctx.fillText(txt, x - ctx.measureText(txt).width / 2, y);
+	}
+	
+	function _drawO(x, y) {
+		ctx.font = "bold "+(font_size+10)+"px Arial";
+		ctx.textBaseline = "middle";
+		var txt = "O";
+		ctx.fillText(txt, x - ctx.measureText(txt).width / 2, y);
+	}
+	
+	function _drawLS(x, y) {
+		ctx.font = "bold "+(font_size+10)+"px Courier";
+		ctx.textBaseline = "middle";
+		var txt = "<";
+		ctx.fillText(txt, x - ctx.measureText(txt).width / 2, y);
+	}
+	
+	function _drawRS(x, y) {
+		ctx.font = "bold "+(font_size+10)+"px Arial";
+		ctx.textBaseline = "middle";
+		var txt = ">";
+		ctx.fillText(txt, x - ctx.measureText(txt).width / 2, y);
 	}
 });
 
 $(document).foundation();
 
-$('.input_show').change(function(){ 
+$('.input_show, .khz_en').change(function(){ 
 	a.Audiogram.reDraw();
 });
 
@@ -186,13 +254,10 @@ $('.slider').on('moved.zf.slider', function(e) {
 
 a.Audiogram.drawLayout();
 
+$('button').click(function() {
+	a.Audiogram.downloadPDF();
+});
 
 
-/*
-var pdf = new jsPDF();
-var width = pdf.internal.pageSize.width;    
-var height = pdf.internal.pageSize.height;
-console.log(width, height / 2);
-pdf.addImage(c.toDataURL("image/png"),"PNG", 0, 0, width, height / 2);
-pdf.save("test.pdf");
-*/
+
+
